a5ed1f7f0ae2343e00716cb78cba2db9acf2b189
tpcollins <tpcollins>
2024-08-12 14:47:51 -0600

    final push for 08/12

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -28,14 +28,16 @@ const Hero: FC = memo(() => {
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({href, text, primary, Icon}) => (
+              {actions.map(({ href, text, primary, Icon }) => (
                 <a
                   className={classNames(
                     'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
-                    primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
+                    primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white'
                   )}
                   href={href}
-                  key={text}>
+                  key={text}
+                  target={href === '/assets/resume/TristanCollinsResume.pdf' ? '_blank' : '_self'}
+                >
                   {text}
                   {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
                 </a>
