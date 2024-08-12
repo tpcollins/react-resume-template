b484df25fd89dbc040d1db704cb472c1e726a417
tpcollins <tpcollins>
2024-08-12 06:56:59 -0600

    home page changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,18 +69,11 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Tristan Collins.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
-      </p>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        I'm a Denver based <strong className="text-stone-100">Full Stack Software Engineer, Web Developer</strong> and <strong className="text-stone-100"> Database Administrator.</strong>
       </p>
     </>
   ),
