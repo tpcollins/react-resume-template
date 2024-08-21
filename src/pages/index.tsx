48f5e38929742f387d6618db251f12ab7a14449a
tpcollins <tpcollins>
2024-08-21 10:23:13 -0600

    temporary edits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
-import Contact from '../components/Sections/Contact';
+// import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
-import Portfolio from '../components/Sections/Portfolio';
+// import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
-import Testimonials from '../components/Sections/Testimonials';
+// import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
@@ -22,9 +22,9 @@ const Home: FC = memo(() => {
       <Hero />
       <About />
       <Resume />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
+      {/* <Portfolio /> */}
+      {/* <Testimonials /> */}
+      {/* <Contact /> */}
       <Footer />
     </Page>
   );
