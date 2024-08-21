2eb1c51eafc6f072d1f0c920b8dc6d9d77394f01
tpcollins <tpcollins>
2024-08-21 08:36:19 -0600

    fixing hero comp for deployment

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -28,20 +28,32 @@ const Hero: FC = memo(() => {
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({ href, text, primary, Icon }) => (
-                <a
-                  className={classNames(
-                    'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
-                    primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white'
-                  )}
-                  href={href}
-                  key={text}
-                  target={href === '/assets/resume/TristanCollinsResume.pdf' ? '_blank' : '_self'}
-                >
-                  {text}
-                  {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
-                </a>
-              ))}
+              {actions.map(({href, text, primary, Icon}) => (
+                // <a
+                //   className={classNames(
+                //     'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
+                //     primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white'
+                //   )}
+                //   href={href}
+                //   key={text}
+                //   target={href === '/assets/resume/TristanCollinsResume.pdf' ? '_blank' : '_self'}
+                // >
+                //   {text}
+                //   {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
+                // </a>
+                  <a
+                    className={classNames(
+                      'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
+                      primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white'
+                    )}
+                    href={href}
+                    key={text}
+                    target={href === '/assets/resume/TristanCollinsResume.pdf' ? '_blank' : '_self'}
+                  >
+                    {text}
+                    {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
+                  </a>
+                ))}
             </div>
           </div>
         </div>
