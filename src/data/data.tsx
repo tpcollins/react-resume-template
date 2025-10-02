4b337e822dca4a2a047c9b03e37390dc69700727
tpcollins <tpcollins00@gmail.com>
2025-10-02 11:35:49 -0600

    forgot to change link name

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -73,7 +73,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/resume/TristanCollinsResume.pdf',
+      href: '/assets/resume/TristanCollinsResumePDF.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
